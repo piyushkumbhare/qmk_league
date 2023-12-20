d1222e30b6ed2e443257fb9038a9bc918165f640
Piyush Kumbhare <piyushkumbhare10@gmail.com>
2023-12-19 20:01:26 -0800

    Added keymaps

---
+++ keyboards/keychron/q3/ansi_encoder/keymaps/league_keymap/rules.mk
@@ -0,0 +1,8 @@
+ENCODER_MAP_ENABLE = yes
+
+VPATH += keyboards/keychron/common
+SRC += keychron_common.c
+
+RGB_MATRIX_ENABLE = yes
+
+RAW_ENABLE = yes
---
+++ keyboards/keychron/q3/ansi_encoder/keymaps/piyushkumbhare/rules.mk
@@ -0,0 +1,9 @@
+# VIA_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+
+VPATH += keyboards/keychron/common
+SRC += keychron_common.c
+
+RGB_MATRIX_ENABLE = yes
+
+RAW_ENABLE = yes
